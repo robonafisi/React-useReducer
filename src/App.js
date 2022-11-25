68c8cfd733b1e585749ab93481f1167eb68525c7
Mohammad Nafisi <nafisidev@gmail.com>
2022-11-25 17:05:16 +0800

    Added list of names

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import {useState, useMemo} from 'react';
 import './App.css';
 
  function App() {
-  const [numbers] = useState([10,20,30]);
+    const [numbers] = useState([10,20,30]);
 
-  const total = numbers.reduce((acc, number) => acc + number, 0);
+    const total = useMemo(() => numbers.reduce((acc, number) => acc + numbers, 0),
+    [numbers]
+    );
+
+    const [names] = useState(["John","Paul","George","Ringo"]);
+
+    const sortNames = [...names].sort();
 
    return(
-     <div>
-     Total: {total}
-     </div>
+    <div>
+       Total: {total}<br></br>
+       {names}<br></br>
+       {sortNames}<br></br>
+    </div>
    );
  }
 
