f7afe23ed313ef073f6b490a8c800b9dd8b02892
Mohammad Nafisi <nafisidev@gmail.com>
2022-11-25 15:48:20 +0800

    Try useMemo

--- src/App.js
+++ src/App.js
@@ -1,78 +1,14 @@
-import { useReducer } from 'react';
- import './App.css';
-
- function UserForm(){
-   const [state, dispatch] = useReducer((state, action) =>({
-       ...state,
-       ...action,
-     }),
-
-   {
-     first:"",
-     last:"",
- });
-   return(
-     <div>
-       <input
-       type="text"
-       value={state.first}
-       onChange={(e)=> dispatch({first: e.target.value})}
-       />
-       <input
-       type="text"
-       value={state.last}
-       onChange={(e)=> dispatch({last: e.target.value})}
-       />
-       <div>
-         First: {state.first}
-       </div>
-       <div>
-           Last: {state.last}
-       </div>
-     </div>
-   );
- }
-
- function NameList() {
-   const[ state, dispatch ] = useReducer((state, action)=>{
-     switch(action.type){
-       case "SET_NAME":
-         return { ...state, name:action.payload};
-       case "ADD_NAME":
-         return {...state, names: [...state.names, state.name ], name:""};
-     }
-   },
-   {
-     names: [],
-     name:"",
-
-   })
-   return(
-     <div>
-       <div>
-         {state.names.map((name, index)=>(
-           <div>{name}</div>
-         ))}
-       </div>
-       <input 
-       type='text'
-       value={state.name}
-       onChange={(e) => dispatch({type:"SET_NAME", payload: e.target.value})}/>
-       <div>
-         <button
-         onClick={() => dispatch({type:"ADD_NAME", payload:state.name})}>
-           Add Name
-         </button>
-       </div>
-     </div>
-   );
- }
+import {useState, useMemo} from 'react';
+import './App.css';
 
  function App() {
+  const [numbers] = useState([10,20,30]);
+
+  const total = numbers.reduce((acc, number) => acc + number, 0);
+
    return(
      <div>
-       <UserForm />
-       <NameList />
+     Total: {total}
      </div>
    );
  }
